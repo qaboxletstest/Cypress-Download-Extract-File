7397f6966e8dd386aaa28ba531fe705ee85692dd
qaboxletstest <qaboxletstest@gmail.com>
2021-06-20 09:55:05 +0530

    Download & Compare

--- cypress/integration/TC_Download_And_Extract.js
+++ cypress/integration/TC_Download_And_Extract.js
@@ -1,12 +1,22 @@
 /// <reference types="Cypress" />
 
 describe('Download File and Assert the content', () => {
-    it('Download & Extract', () => {
+    it('Download & Extract Text', () => {
         cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg', 'mydownloads', 'example.jpg')
             .then(() => {
                 cy.task("getImageText", { fileName: "./mydownloads/example.jpg", lang: "eng", logger: false })
                     .then(text => {
                         expect(text).to.contains("Are you enjoying Cypress")
+                    })
+            })
+    });
+
+    it('Download & Compare Images', () => {
+        cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg', 'mydownloads', 'example.jpg')
+            .then(() => {
+                cy.task("compareImages", { fileName1: "./mydownloads/example.jpg", fileName2: "DifferentImage.jpg" })
+                    .then(isMatching => {
+                        expect(isMatching).to.be.true;
                     })
             })
     });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,12 @@
 const { downloadFile } = require('cypress-downloadfile/lib/addPlugin')
 const tesseract = require("tesseract.js")
 const fs = require("fs")
+const Jimp = require("jimp");
 module.exports = (on, config) => {
   on('task', {
     downloadFile: downloadFile,
-    getImageText: getImageText
+    getImageText: getImageText,
+    compareImages: compareImages
   })
   return config;
 }
@@ -28,5 +30,21 @@ const getImageText = async (obj) => {
     }
   } catch (error) {
     return error.message
+  }
+}
+
+const compareImages = async (obj) => {
+  const { fileName1, fileName2 } = obj
+  const example1 = await Jimp.read(fileName1)
+  const example2 = await Jimp.read(fileName2)
+  const example1Hash = example1.hash()
+  const example2Hash = example2.hash()
+  const distance = Jimp.distance(example1, example2)
+  const diff = Jimp.diff(example1, example2)
+
+  if (example1Hash !== example2Hash || distance > 0.15 || diff > 0.15) {
+    return false
+  } else {
+    return true
   }
 }
